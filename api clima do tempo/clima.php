d64671042d77d239e6e98eb1b8154231a35f275b
Maria Fernanda Gonçalves <91507393+nandinhaaa@users.noreply.github.com>
2024-08-04 13:29:31 -0300

    Add files via upload

---
+++ api clima do tempo/clima.php
@@ -0,0 +1,59 @@
+﻿<!DOCTYPE html>
+<html lang="pt-br">
+<head> 
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+  <link href="css/style.css" rel="stylesheet" type="text/css" /> 
+  <title>Dados do Clima</title>
+</head>
+<body>
+  <header>
+    <h1>Dados do Clima</h1>
+  </header>
+
+  <section>
+    <div id="cityInfo">
+      <?php
+       
+      $cidadeId = $_POST['cidade_id'];
+      $url = "http://servicos.cptec.inpe.br/XML/cidade/$cidadeId/previsao.xml";
+
+      $xmlContent = file_get_contents($url);
+
+      if($xmlContent === FALSE){
+
+        die('Erro ao acessar API');
+      }
+
+      $xml = simplexml_load_string($xmlContent);
+
+      if($xml === FALSE){
+        die('Erro ao carregar xml');
+      }
+
+      //Exibindo as informações
+      echo "<h1>Previsão de tempo para ".htmlspecialchars($xml->nome). " - ".htmlspecialchars($xml->uf)."</h1>"; 
+
+      echo "<table border=1>";
+      echo "<tr><th>Data</th><th>Máxima (°C)</th><th>Mínima (°C)</th>";
+      foreach($xml->previsao as $previsao){
+        $data = new DateTime($previsao->dia);
+        $dataFormatada = $data->format('d/m/y');
+
+
+        echo "<tr>";
+        echo "<td>".htmlspecialchars($dataFormatada)."</td>";
+        echo "<td>".htmlspecialchars($previsao->maxima)."</td>";
+        echo "<td>".htmlspecialchars($previsao->minima)."</td>";
+      echo "</tr>";
+    }
+
+  ?>
+</div>
+
+<form action="index.php" method="get">
+  <input type="submit" value="Voltar para a página inicial" class="botao">
+</form>
+</section>
+</body>
+</html>
---
+++ api clima do tempo/index.php
@@ -0,0 +1,61 @@
+﻿<?php
+require_once("localidade.php");
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="css/style.css" rel="stylesheet" type="text/css" />
+  <!-- Inclua o jQuery -->
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+  <title>Consumindo API</title>
+</head>
+<body>
+  <header>
+    <h1>Consulta Localidade</h1>
+  </header>
+  <section>
+    <!-- O atributo accept-charset define a codificação de caracteres que o navegador deve usar ao enviar os dados do formulário para o servidor. Ao definir accept-charset="UTF-8", você está garantindo que o navegador envie os dados do formulário no formato UTF-8. -->
+  
+    <form id="cityForm" action="." method="POST" accept-charset="UTF-8">
+      <label for="nome">Digite o nome da cidade</label>
+      <input type="text" required="required" name="city" id="city" size="40">
+      <br>
+      <input type="submit" value="Consultar" class="botao"/>
+      <br>
+    </form>
+    <div id="cityInfo">
+      
+    </div>
+  </section>
+
+  <script>
+    $(document).ready(function() {
+      $('#cityForm').submit(function(event) {
+        event.preventDefault(); // Previne o envio normal do formulário
+        
+        var cityName = $('#city').val();
+        
+        $.post('localidade.php', { city: cityName }, function(response) {
+          var data = JSON.parse(response);
+          
+          var output = '<h2>Informações da Cidade:</h2>';
+          $(data.cidade).each(function(index, cidade) {
+            output += '<p><strong>Nome:</strong> ' + cidade.nome + '</p>';
+            output += '<p><strong>UF:</strong> ' + cidade.uf + '</p>';
+            output += '<p><strong>ID:</strong> ' + cidade.id + '</p>';
+            output += '<form action="clima.php" method="post">';
+            output += '<input type="hidden" name="cidade_id" value="' + cidade.id + '">';
+            output += '<input type="submit" value="Ver Clima" class="botao">';
+            output += '</form>';
+            output += '<hr>';
+          });
+          
+          $('#cityInfo').html(output);
+        });
+      });
+    });
+  </script>
+</body>
+</html>
